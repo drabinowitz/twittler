f4d9edb2b10b26fdec425532da2853b32c2fe584
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-09-10 15:34:30 -0400

    working on adding messages to DOM

--- twittler.js
+++ twittler.js
@@ -1,14 +1,41 @@
 $(document).ready(function(){
-var $body = $('body');
-$body.html('');
+  
+  var $body = $('body');
+  
+  $body.html('');
+  
+  var lastUpdate='0';
 
-var index = streams.home.length - 1;
-while(index >= 0){
-  var tweet = streams.home[index];
-  var $tweet = $('<div></div>');
-  $tweet.text('@' + tweet.user + ': ' + tweet.message);
-  $tweet.appendTo($body);
-  index -= 1;
-}
+  function appendTweetToDom(){
+  
+    var tweetsToAppend = _.filter(streams.home,function(tweet){
+
+      return moment(tweet.created_at).isAfter(moment(lastUpdate));
+
+    });
+  
+    lastUpdate = streams.home[0].created_at;
+  
+    var index = tweetsToAppend.length - 1;
+  
+    while(index >= 0){
+  
+      var tweet = tweetsToAppend[index];
+  
+      var $tweet = $('<div></div>');
+  
+      $tweet.text('@' + tweet.user + ': ' + tweet.message + '  added: ' + moment(tweet.created_at).fromNow());
+  
+      $tweet.appendTo($body);
+  
+      index -= 1;
+  
+    }
+  
+  }
+
+  appendTweetToDom();
+
+  setInterval(appendTweetToDom,5000);
 
 });
