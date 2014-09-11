5139e8e5d674f483a7f0e5921ebe7744a456f5b6
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-09-11 12:51:19 -0400

    add tweet button

--- twittler.js
+++ twittler.js
@@ -1,4 +1,6 @@
-var username = prompt('Username: ');
+var username = prompt('Username: ','defaultUser');
+
+streams.users[username] = [];
 
 $(document).ready(function(){
   
@@ -8,11 +10,155 @@ $(document).ready(function(){
 
   var tweetsOnDom = [];
 
+  function buildTweet(tweet){
+
+    var $tweet = $('<div></div>').attr('class','tweet-wrapper');
+
+    if (tweet.guid !== undefined){
+
+      $tweet.attr('id',tweet.guid);
+
+    }
+
+    var $tweetBody = $('<h5></h5>').attr('class','tweet-body');
+
+    var $tweetTs = $('<h6></h6>').attr('class','tweet-ts');
+
+    var $tweetUser = $('<a></a>').attr({
+
+      'class':'tweet-user',
+
+      'href' : '#'
+
+    }).append('@' + tweet.user);
+
+    $tweetBody.append($tweetUser).append(': ' + tweet.message);
+
+    $tweetTs.text('added: ' + tweet.created_at);
+
+    $tweetBody.appendTo($tweet);
+
+    $tweetTs.appendTo($tweet);
+
+    return $tweet;
+
+  }
+
   function getRelativeTweetTs(tweetTs){
 
     return moment(tweetTs).fromNow();
 
   }
+
+  var updateUserTweets = (function(){
+
+    var intervalId;
+
+    var $body = $('.twittler-user-stream-tweets');
+    
+    var $twittlerUser = $('.twittler-user');
+
+    var $twittlerButton = $('.hide-twittler-user-stream');
+
+    return { 
+
+      add : function(user){
+
+        this.remove();
+
+        $twittlerButton.show();
+
+        $twittlerUser.text(user);
+
+        function addUserStreamsToDom(){
+
+          var index = 0;
+
+          var userStream = streams.users[user];
+
+          while(index <= userStream.length - 1){
+
+            var tweet = userStream[index];
+
+            var $tweet = buildTweet(tweet);
+
+            $tweet.prependTo($body);
+
+            index++;
+
+          }
+
+        }
+
+        intervalId = setInterval(addUserStreamsToDom,60000);
+
+        addUserStreamsToDom();      
+
+      },
+
+      remove : function(){
+
+        window.clearInterval(intervalId);
+
+        $body.empty();
+
+        $twittlerUser.text('');
+
+        $twittlerButton.hide();
+
+      }
+
+    };
+
+  })();
+
+  $('.twittler-content').on('click','.tweet-user',function(event){
+
+    event.preventDefault();
+
+    updateUserTweets.add( $(this).text().substring(1) );
+
+  });
+
+  $('.twittler-content').on('click','.hide-twittler-user-stream',function(event){
+
+    event.preventDefault();
+
+    updateUserTweets.remove();
+
+  });
+
+  $('.add-tweet-button').click(function(event){
+
+    event.preventDefault();
+
+    var tweetText = $(this).closest('.add-tweet').find('.add-tweet-text');
+
+    if (username && tweetText.val() && tweetText.val() != ''){
+
+      var tweet = {
+
+        user : username,
+
+        message : tweetText.val(),
+
+        created_at : moment()._d
+
+      };
+
+      tweetText.val('');
+
+      streams.home.push(tweet);
+
+      streams.users[username].push(tweet);
+      
+    } else {
+
+      $('.error-text').fadeIn('fast').delay(2000).fadeOut('fast');
+
+    }
+
+  });
 
   function updateTweetsTs(){
 
@@ -49,26 +195,8 @@ $(document).ready(function(){
       tweetsOnDom.push(tweet);
 
       tweet.guid = tweetsOnDom.length - 1;
-  
-      var $tweet = $('<div></div>').attr({
 
-        'class':'tweet-wrapper',
-
-        'id': tweet.guid
-
-      });
-
-      var $tweetBody = $('<h5></h5>').attr('class','tweet-body');
-
-      var $tweetTs = $('<h6></h6>').attr('class','tweet-ts');
-
-      $tweetBody.text('@' + tweet.user + ': ' + tweet.message);
-
-      // $tweetTs.text('added: ' + getRelativeTweetTs(tweet.created_at));
-  
-      $tweetBody.appendTo($tweet);
-
-      $tweetTs.appendTo($tweet);
+      var $tweet = buildTweet(tweet);
   
       $tweet.prependTo($body);
   
